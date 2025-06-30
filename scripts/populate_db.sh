40b6bae39bf5f84348988c13285873e75829038c
Anton Chernov <tony.pitchblack@yandex.ru>
2025-06-30 16:46:24 +0300

    use all rows to populate db by default

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -10,6 +10,5 @@ cd ~/rl4mm/rl4mm/database && \
 python3 populate_database.py \
   --path_to_lobster_data "../../test_data" \
   --book_snapshot_freq "S" \
-  --max_rows 1000 \
   --ticker MSFT \
   --infer_dates_from_files
